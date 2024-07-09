1abea8c916f0fa44e7c149498506489d0a236023
franciscoGermani <franciscogermani98@gmail.com>
2024-07-09 19:33:00 -0300

    enviromentCreation

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    // Aquí puedes añadir otras variables de entorno que necesites
+  };
+  
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    url: 'http://localhost:3000/' // Cambia esto a la URL de tu API de desarrollo
+  };
+  
